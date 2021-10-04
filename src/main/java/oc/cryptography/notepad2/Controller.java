e3e1bdf35e747513218d33c0151799a410b90071
David Burris <david.burris@ou.edu>
2021-10-04 12:35:37 -0500

    Implemented basic file save and open

--- src/main/java/oc/cryptography/notepad2/Controller.java
+++ src/main/java/oc/cryptography/notepad2/Controller.java
@@ -2,12 +2,17 @@ package oc.cryptography.notepad2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Controller {
 
@@ -25,8 +30,8 @@ public class Controller {
     @FXML
     private MenuItem encryptedSaveOption;
 
-    static final int ENCRYPTED_TAG = 0xA5;
-    static final int UNENCRYPTED_TAG = 0xFF;
+    static final byte ENCRYPTED_TAG =   (byte)0xA5;
+    static final byte UNENCRYPTED_TAG = (byte)0xFF;
 
     void setStage(Stage stage) {
         this.stage = stage;
@@ -44,17 +49,23 @@ public class Controller {
         dialog.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text File","*.txt"));
         File file = dialog.showSaveDialog(stage);
         if (file != null) {
-            handleSavingFile(file);
+            try {
+                handleSavingFile(file);
+            } catch (IOException e) {
+                new Alert(Alert.AlertType.ERROR, "Error saving file. Aborting.").showAndWait();
+                return;
+            }
         }
     }
 
-    private void handleSavingFile(File file) {
-        //TODO Create output byte array with tag (buffer?)
-
-        //TODO Append text area text as a byte array
-        textArea.getText();
-
-        //TODO Write bytes into file and close
+    private void handleSavingFile(File file) throws IOException {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        byteStream.write(UNENCRYPTED_TAG);
+        byteStream.writeBytes(textArea.getText().getBytes());
+        FileOutputStream fileStream = new FileOutputStream(file);
+        byteStream.writeTo(fileStream);
+        byteStream.close();
+        fileStream.close();
     }
 
     @FXML
@@ -88,28 +99,36 @@ public class Controller {
         dialog.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text File","*.txt"));
         File file = dialog.showOpenDialog(stage);
         if (file != null) {
-            handleOpeningFile(file);
+            try {
+                handleOpeningFile(file);
+            } catch (IOException e) {
+                new Alert(Alert.AlertType.ERROR, "Error opening file. Aborting.").showAndWait();
+                return;
+            }
         }
     }
 
-    private void handleOpeningFile(File file) {
-        //TODO Create FileInputStream from file
+    private void handleOpeningFile(File file) throws IOException {
+        FileInputStream fileStream = new FileInputStream(file);
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 
-        //TODO Create empty byte array for textArea content (buffer?)
+        byte tag = (byte)fileStream.read();
 
-        //TODO Read first byte to see if encrypted
-
-        //TODO If encrypted,
+        if (tag == ENCRYPTED_TAG) {
             //TODO Request and store password
             //TODO Read and store next 32 bytes as password hash
             //TODO If provided password hashes to same value,
                 //TODO Decrypt and append remaining bytes
             //TODO else, open an Alert stating password was incorrect and the operation was aborted
-                //TODO return
+            //TODO return
+        } else {
+            byteStream.writeBytes(fileStream.readAllBytes());
+        }
 
-        //TODO else, append bytes into byte array
-
-        //TODO Write byte array into textArea
+        fileStream.close();
+        textArea.clear();
+        textArea.setText(byteStream.toString());
+        byteStream.close();
     }
 
 }
